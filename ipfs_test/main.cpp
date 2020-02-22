70d0bf7026379d93fe36904552509704d2fe768b
tony92151 <tony92151@gmail.com>
2020-02-22 04:06:27 +0000

    add function

--- ipfs_test/main.cpp
+++ ipfs_test/main.cpp
@@ -3,9 +3,21 @@ using namespace eosio;
 class [[eosio::contract]] ipfs : public contract {
     public:
         using contract::contract;
-        [[eosio::action]] void upload(){
+        [[eosio::action]] void registe(){
             print("Hello ",get_self());
             print("Hello ",get_first_receiver());
         }
+
+    
+    private:
+        using contract::contract;
+        [[eosio::action]] void encrypt(){
+            
+        }
+
+        using contract::contract;
+        [[eosio::action]] void upload(){
+            
+        }
     };
-EOSIO_DISPATCH( ipfs, (upload) )
+EOSIO_DISPATCH( ipfs, (registe)(encrypt)(upload))
